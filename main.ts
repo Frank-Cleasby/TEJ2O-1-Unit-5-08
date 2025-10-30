f6f133ac7261973352e2355384acab04560671a4
Frank-Cleasby <frank.cleasby@stu.ocsb.ca>
2025-10-30 13:29:06 -0400

    some works sometwhat

--- main.ts
+++ main.ts
@@ -1,8 +1,31 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Frank
+ * Created on: Oct 2025
+ * This program moves a cool car
 */
 
-basic.showString('Hello, World!')
+// varables
+let distanceOfObstacle: number = 0
+
+// clean
+basic.clearScreen()
+basic.showIcon(IconNames.Giraffe)
+
+// car start
+while (true) {
+    distanceOfObstacle = sonar.ping(
+        DigitalPin.P12, 
+        DigitalPin.P13, 
+        PingUnit.Centimeters)
+    if (distanceOfObstacle > 10) {
+        robotbit.StpCarMove(10, 48)
+        distanceOfObstacle = sonar.ping(
+            DigitalPin.P12,
+            DigitalPin.P13,
+            PingUnit.Centimeters)
+    }
+    basic.pause(1000)
+    robotbit.StpCarMove(-10, 48)
+    robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B4)
+}
